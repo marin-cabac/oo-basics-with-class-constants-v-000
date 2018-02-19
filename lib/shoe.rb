dc7a0b70a1c66f9945c5a37394c3acef3244d480
Marin Cabac <marin.cabac@gmail.com>
2018-02-19 01:31:04 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,8 +5,8 @@ class Shoe
   def initialize(brand)
     @brand = brand
 
-          BRANDS << brand if !(BRANDS.include?(brand))
-
+        #  BRANDS << brand if !(BRANDS.include?(brand))
+!(BRANDS.include?(brand)) ? BRANDS << brand : next
         
 
   end
