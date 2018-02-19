a5938acf954e7b038af12105f540850a623208bf
Marin Cabac <marin.cabac@gmail.com>
2018-02-19 01:29:05 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,7 +4,10 @@ class Shoe
  BRANDS  = []
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+     
+       if !(BRANDS.include?(brand)) then BRANDS << brand
+       end
+  
   end
 
   def cobble
